8f62800adbfa0dab432954dd7a49e491580b067d
viztra <dwighttraviz@gmail.com>
2025-05-10 19:19:38 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something To Tell You'.split('')
+const text = 'I Have Something For You'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
